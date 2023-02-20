23fd3f76da4460df393553a64e4872a817402adf
Jarod Burchill <jarod.burchill@thescore.com>
2023-02-20 09:29:07 -0700

    Initial commit

---
+++ apps/myapp/src/app/app.tsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="myapp" />
+
+      <div />
+    </>
+  );
+}
+
+export default App;
